992ffad7c4f1e5177466a6148bbb0639e3465cdf
david <d.long@tenum.ai>
2024-12-31 16:49:36 +0800

    commit for deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,6 @@ interface User {
 export default function Home() {
   const supabase = createClient();
   const router = useRouter();
-
   const today = new Date();
   const currentDay = today.getDay(); 
   const currentWeekMonday = new Date(today);
