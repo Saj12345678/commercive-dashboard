b03c86b52ee5fff4b0d6e1af0f28d492a88d6264
Great Dev <twentytwentyaugust@outlook.com>
2025-03-13 06:20:51 -0500

    feat: update admin/home and admin/inventory

--- src/app/admin/inventory/page.tsx
+++ src/app/admin/inventory/page.tsx
@@ -1,5 +1,5 @@
-import Inventory from "@/components/admin/inventory";
 import React from "react";
+import Inventory from "@/components/admin/inventory";
 
 export default function InventoryPage() {
   return <Inventory />;
