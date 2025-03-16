5ab09dd3cfd11a899ef2f24de66697210c11d888
Great Dev <twentytwentyaugust@outlook.com>
2025-03-15 21:30:41 -0400

    feat: add admin/payout page

--- src/app/admin/payouts/page.tsx
+++ src/app/admin/payouts/page.tsx
@@ -1,5 +1,5 @@
-import Payout from "@/components/admin/payout";
 import React from "react";
+import Payout from "@/components/admin/payout";
 
 export default function PayoutPage() {
   return <Payout />;
